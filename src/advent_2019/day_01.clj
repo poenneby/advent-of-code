37861d6a1b58d228cf710c2f39d08b2db829c2ba
Peter Onneby <ponneby@xebia.fr>
2019-12-01 23:55:46 +0100

    Day 1 Part 2

--- src/advent_2019/day_01.clj
+++ src/advent_2019/day_01.clj
@@ -3,9 +3,18 @@
    [clojure.java.io :as io]
    [clojure.edn :as edn]))
 
-(defn calculate [module-mass] 
+(defn calculate-fuel [module-mass] 
+  "Calculates fuel required to launch a given module based on its' mass"
   (- (int (Math/floor (/ module-mass 3))) 2))
 
-(with-open [rdr (io/reader "resources/advent_2019/input")]
-  (reduce + (map calculate (map edn/read-string (line-seq rdr)))))
 
+(defn fuel-required-for [fuel] 
+  (let [fuel-required (calculate-fuel fuel)]
+    (loop [sum 0
+           f fuel-required]
+      (if (< f 0)
+        sum
+        (recur (+ sum f) (calculate-fuel f))))))
+
+(with-open [rdr (io/reader "resources/advent_2019/input")]
+  (reduce + (map fuel-required-for (map edn/read-string (line-seq rdr)))))

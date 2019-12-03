de10f530f5939b2634302b85f1c36c731aede8bf
Peter Onneby <ponneby@xebia.fr>
2019-12-03 14:23:34 +0100

    Day 2, part 1

---
+++ src/advent_2019/day_02.clj
@@ -0,0 +1,30 @@
+(ns advent-2019.day_02
+  (:require
+   [clojure.string :as str]
+   [clojure.edn :as edn]))
+
+
+(def data (map edn/read-string (str/split (slurp "resources/advent_2019/day_2/input") #",")))
+
+(defn op-from [opcode]
+  (if (= 1 opcode) + *))
+
+(defn replace-at-position-with-operation [position operation value1 value2 input-list]
+  (assoc input-list position (operation value1 value2)))
+
+(defn calculate [input] 
+ (loop [offset 0
+        i input] 
+   (let [opcode (nth i (+ 0 offset))] 
+     (if (= 99 opcode)
+     i
+     (let [pos1 (nth i (+ 1 offset))
+         pos2 (nth i (+ 2 offset))
+         replace-pos (nth i (+ 3 offset))]
+    (recur (+ offset 4) (replace-at-position-with-operation replace-pos (op-from opcode) (nth i pos1) (nth i pos2) i))))))) 
+
+(defn restore-gravity [data]
+  (assoc (assoc data 1 12) 2 2))
+
+(calculate (restore-gravity (into [] (doall data))))
+
